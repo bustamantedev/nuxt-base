1b6ede1e67b0986ffcd2692b1bd9665bbf74eb6f
Orlando Bustamante <obustamante@netflix.com>
2024-04-28 17:00:24 -0700

    chore: add tailwind config path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,6 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: [join(currentDir, './assets/main.css')],
+  css: [join(currentDir, './assets/css/tailwind.css')],
   modules: ["@nuxtjs/tailwindcss"]
 })
